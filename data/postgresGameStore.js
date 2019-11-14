562c05e2853ea41dc9e3612cc5af63f01a234c32
rscampbell333 <1151216+rscampbell333@users.noreply.github.com>
2019-11-13 22:19:04 -0600

    add handling for away team

--- data/postgresGameStore.js
+++ data/postgresGameStore.js
@@ -32,35 +32,39 @@ class PostgresDataStore {
     const client = await pool.connect();
 
     try {
-      const selectGame = 'SELECT date, home_team FROM games WHERE id = $1';
+      const selectGame = 'SELECT date, home_team, away_team FROM games WHERE id = $1';
       const values = [id];
 
       const gameRows = await client.query(selectGame, values);
 
       if (gameRows.rows.length > 0) {
         const row = gameRows.rows[0];
-        const game = { id, metadata: { teamName: row.home_team, date: row.date } };
+        const game = { id, metadata: { homeTeam: row.home_team, awayTeam: row.away_team, date: row.date } };
 
-        const selectAtBats = 'SELECT inning, balls, strikes, position, result, farthest_base FROM at_bats WHERE game_id = $1';
+        const selectAtBats = 'SELECT inning, balls, strikes, position, result, farthest_base, home FROM at_bats WHERE game_id = $1';
         const atBats = await client.query(selectAtBats, values);
 
-        const positions = Array.from({ length: 9 }, (x, i) => ({ position: i + 1, results: [], players: [] }));
+        const homePositions = Array.from({ length: 9 }, (x, i) => ({ position: i + 1, results: [], players: [] }));
+        const awayPositions = Array.from({ length: 9 }, (x, i) => ({ position: i + 1, results: [], players: [] }));
         for (const atBatRow of atBats.rows) {
-          const { inning, result, balls, strikes, position, farthest_base: farthestBase } = atBatRow;
+          const { inning, result, balls, strikes, position, farthest_base: farthestBase, home } = atBatRow;
           const atBat = { inning, result, farthestBase, count: { balls, strikes } };
+
+          const positions = home ? homePositions : awayPositions;
 
           positions[position - 1].results[inning - 1] = atBat;
         }
 
-        const selectPositions = 'SELECT player, position, since FROM batting_positions WHERE game_id = $1';
+        const selectPositions = 'SELECT player, position, since, home FROM batting_positions WHERE game_id = $1';
         const positionRows = await client.query(selectPositions, values);
 
         for (const positionRow of positionRows.rows) {
-          const { player: name, since, position } = positionRow;
+          const { player: name, since, position, home } = positionRow;
+          const positions = home ? homePositions : awayPositions;
           positions[position - 1].players.push({ name, since });
         }
 
-        game.innings = positions;
+        game.innings = { home: homePositions, away: awayPositions };
         return game;
       }
     } catch (err) {
@@ -76,28 +80,34 @@ class PostgresDataStore {
 
     try {
       const insert = 'INSERT INTO games VALUES ($1, $2, $3, $4)';
-      const values = [game.id, game.metadata.date, game.metadata.teamName, null];
+      const values = [game.id, game.metadata.date, game.metadata.homeTeam, game.metadata.awayTeam];
       debug(`Creating game meta data with query ${insert} and values ${values}`);
       await client.query('BEGIN');
       await client.query(insert, values);
 
-      const insertAtBat = 'INSERT INTO at_bats VALUES ($1, $2, $3, $4, $5, $6, $7)';
-      const insertPlayer = 'INSERT INTO batting_positions VALUES ($1, $2, $3, $4)';
-      for (const position of game.innings) {
-        if (position.results) {
-          for (const atBat of position.results) {
-            if (atBat && atBat.inning && atBat.count && atBat.result && atBat.farthestBase) {
-              const atBatValues = [game.id, atBat.inning, atBat.count.balls, atBat.count.strikes, position.position, atBat.result, atBat.farthestBase];
-              await client.query(insertAtBat, atBatValues);
+      const insertAtBat = 'INSERT INTO at_bats(game_id, inning, balls, strikes, position, result, farthest_base, home) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
+      const insertPlayer = 'INSERT INTO batting_positions(game_id, position, player, since, home) VALUES ($1, $2, $3, $4, $5)';
+      
+      for (const team of ['home', 'away']) {
+        const innings = game.innings[team];
+        const isHome = team === 'home';
+        for (const position of innings) {
+          if (position.results) {
+            for (const atBat of position.results) {
+              if (atBat && atBat.inning && atBat.count && atBat.result) {
+                const farthestBase = atBat.farthestBase || 0;
+                const atBatValues = [game.id, atBat.inning, atBat.count.balls, atBat.count.strikes, position.position, atBat.result, farthestBase, isHome];
+                await client.query(insertAtBat, atBatValues);
+              }
             }
           }
-        }
 
-        if (position.players) {
-          for (const player of position.players) {
-            const { name, since } = player;
-            const playerValues = [game.id, position.position, name, since];
-            await client.query(insertPlayer, playerValues);
+          if (position.players) {
+            for (const player of position.players) {
+              const { name, since } = player;
+              const playerValues = [game.id, position.position, name, since, isHome];
+              await client.query(insertPlayer, playerValues);
+            }
           }
         }
       }
@@ -116,8 +126,8 @@ class PostgresDataStore {
     const client = await pool.connect();
 
     try {
-      const updateGame = 'UPDATE games SET date = $1, home_team = $2 WHERE id = $3';
-      const values = [new Date(game.metadata.date), game.metadata.teamName, id];
+      const updateGame = 'UPDATE games SET date = $1, home_team = $2, away_team = $3 WHERE id = $4';
+      const values = [new Date(game.metadata.date), game.metadata.homeTeam, game.metadata.awayTeam, id];
       debug(`Updating game metadata with query ${updateGame} and values ${values}`);
       await client.query('BEGIN');
       await client.query(updateGame, values);
@@ -127,23 +137,29 @@ class PostgresDataStore {
       const deleteOldPositions = 'DELETE FROM batting_positions WHERE game_id = $1';
       client.query(deleteOldPositions, [id]);
 
-      const insertAtBat = 'INSERT INTO at_bats VALUES ($1, $2, $3, $4, $5, $6, $7)';
-      const insertPlayer = 'INSERT INTO batting_positions VALUES ($1, $2, $3, $4)';
-      for (const position of game.innings) {
-        if (position.results) {
-          for (const atBat of position.results) {
-            if (atBat && atBat.inning && atBat.count && atBat.result && atBat.farthestBase) {
-              const atBatValues = [id, atBat.inning, atBat.count.balls, atBat.count.strikes, position.position, atBat.result, atBat.farthestBase];
-              await client.query(insertAtBat, atBatValues);
+      const insertAtBat = 'INSERT INTO at_bats(game_id, inning, balls, strikes, position, result, farthest_base, home) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
+      const insertPlayer = 'INSERT INTO batting_positions(game_id, position, player, since, home) VALUES ($1, $2, $3, $4, $5)';
+      for (const team of ['home', 'away']) {
+        const innings = game.innings[team];
+        const isHome = team === 'home';
+
+        for (const position of innings) {
+          if (position.results) {
+            for (const atBat of position.results) {
+              if (atBat && atBat.inning && atBat.count && atBat.result) {
+                const { farthestBase = 0 } = atBat;
+                const atBatValues = [id, atBat.inning, atBat.count.balls, atBat.count.strikes, position.position, atBat.result, farthestBase, isHome];
+                await client.query(insertAtBat, atBatValues);
+              }
             }
           }
-        }
 
-        if (position.players) {
-          for (const player of position.players) {
-            const { name, since } = player;
-            const playerValues = [id, position.position, name, since];
-            await client.query(insertPlayer, playerValues);
+          if (position.players) {
+            for (const player of position.players) {
+              const { name, since } = player;
+              const playerValues = [id, position.position, name, since, isHome];
+              await client.query(insertPlayer, playerValues);
+            }
           }
         }
       }
